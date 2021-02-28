ff7b1ed82a22da57a504d47b3c13ae445c13ffe7
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-02-28 19:48:38 +0200

    add checkout form

---
+++ src/components/Checkout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const steps = ["Shipping Addres", "Payment Details"];
+
+const Checkout = () => {
+  return (
+    <div className="checkout">
+      <h2 className="checkout__title">Checkout</h2>
+    </div>
+  );
+};
+
+export default Checkout;
