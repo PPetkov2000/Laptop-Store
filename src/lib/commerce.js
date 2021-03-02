9bfd59aeca41d7fb065b0b08b5004a5830ed1da6
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-03-02 23:46:25 +0200

    Update commerce.js

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,6 +1,6 @@
 import Commerce from "@chec/commerce.js";
 
 export const commerce = new Commerce(
-  "pk_23809368897c90767dbe570c71c2278fc2b17e442d618",
+  process.env.REACT_APP_CHEC_PUBLIC_API_KEY,
   true
 );
