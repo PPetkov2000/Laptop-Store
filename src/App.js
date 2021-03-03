20ed9e301482dfcfdc602ec4d7fc8d69db7263ba
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-03-03 23:22:20 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,59 +12,56 @@ import { commerce } from "./lib/commerce";
 
 function App() {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState();
+  const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
 
   const fetchProducts = async () => {
-    try {
-      const { data } = await commerce.products.list();
-      setProducts(data);
-    } catch (error) {
-      console.log(error);
-    }
+    const { data } = await commerce.products.list();
+    setProducts(data);
   };
 
   const fetchCart = async () => {
-    try {
-      const data = await commerce.cart.retrieve();
-      setCart(data);
-    } catch (error) {
-      console.log(error);
-    }
+    const data = await commerce.cart.retrieve();
+    setCart(data);
   };
 
   const handleAddToCart = async (productId, quantity) => {
-    try {
-      const { cart } = await commerce.cart.add(productId, quantity);
-      setCart(cart);
-    } catch (error) {
-      console.log(error);
-    }
+    const { cart } = await commerce.cart.add(productId, quantity);
+    setCart(cart);
   };
 
   const handleRemoveItem = async (productId) => {
-    try {
-      const { cart } = await commerce.cart.remove(productId);
-      setCart(cart);
-    } catch (error) {
-      console.log(error);
-    }
+    const { cart } = await commerce.cart.remove(productId);
+    setCart(cart);
   };
 
   const handleUpdateCartQuantity = async (productId, quantity) => {
-    try {
-      const { cart } = await commerce.cart.update(productId, { quantity });
-      setCart(cart);
-    } catch (error) {
-      console.log(error);
-    }
+    const { cart } = await commerce.cart.update(productId, { quantity });
+    setCart(cart);
   };
 
   const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+    setCart(cart);
+  };
+
+  const refreshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+    setCart(newCart);
+  };
+
+  const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
     try {
-      const { cart } = await commerce.cart.empty();
-      setCart(cart);
+      const incomingOrder = await commerce.checkout.capture(
+        checkoutTokenId,
+        newOrder
+      );
+      setOrder(incomingOrder);
+      refreshCart();
     } catch (error) {
       console.log(error);
+      setErrorMessage(error.data.error.message);
     }
   };
 
@@ -87,12 +84,18 @@ function App() {
           <Route path="/cart" exact>
             <Cart
               cart={cart}
+              handleUpdateCartQuantity={handleUpdateCartQuantity}
               handleRemoveItem={handleRemoveItem}
               handleEmptyCart={handleEmptyCart}
             />
           </Route>
           <Route path="/checkout" exact>
-            <Checkout cart={cart} />
+            <Checkout
+              cart={cart}
+              order={order}
+              onCaptureCheckout={handleCaptureCheckout}
+              error={errorMessage}
+            />
           </Route>
           <Route path="*" component={PageNotFound} />
         </Switch>
