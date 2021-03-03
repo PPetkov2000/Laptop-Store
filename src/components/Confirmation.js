e237ec1536fbbdedc93834b9c0d7e617a77b25ab
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-03-03 23:21:13 +0200

    Update Confirmation.js

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Loader from "./Loader";
 
-const Confirmation = () => {
-  return (
-    <div>
-      <h1>Confirmation</h1>
+const Confirmation = ({ order, error }) => {
+  return !order.customer ? (
+    <Loader />
+  ) : error ? (
+    <div className="confirmation">
+      <h5 className="confirmation__error">{error}</h5>
+      <Link to="/" className="confirmation__link">
+        Back to home
+      </Link>
+    </div>
+  ) : (
+    <div className="confirmation">
+      <h5 className="confirmation__title">
+        Thank you for your purchase, {order.customer.firstname}{" "}
+        {order.customer.lastname}
+      </h5>
+      <p className="confirmation__text">
+        Order ref: {order.customer_reference}
+      </p>
+      <Link to="/" className="confirmation__link">
+        Back to home
+      </Link>
     </div>
   );
 };
