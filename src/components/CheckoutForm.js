d407f5de5607ec29b1be8318d27ae3be8c801696
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-03-03 23:21:35 +0200

    Update CheckoutForm.js

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -4,6 +4,12 @@ import { commerce } from "../lib/commerce";
 import FormInput from "./FormInput";
 
 const CheckoutForm = ({ checkoutToken, setShippingData, nextStep }) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
   const [shippingCountries, setShippingCountries] = useState([]);
   const [shippingCountry, setShippingCountry] = useState("");
   const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
@@ -40,7 +46,7 @@ const CheckoutForm = ({ checkoutToken, setShippingData, nextStep }) => {
       }
     );
     setShippingOptions(options);
-    setShippingOption(options[0].id);
+    setShippingOption(options[0]?.id);
   };
 
   useEffect(() => {
@@ -58,74 +64,119 @@ const CheckoutForm = ({ checkoutToken, setShippingData, nextStep }) => {
         shippingCountry,
         shippingSubdivision
       );
-  }, [shippingSubdivision]);
+  }, [checkoutToken, shippingCountry, shippingSubdivision]);
 
-  const handleSubmit = (data) => {
+  const handleSubmit = () => {
     setShippingData({
-      ...data,
+      firstName,
+      lastName,
+      email,
+      address,
+      city,
+      postalCode,
       shippingCountry,
       shippingSubdivision,
       shippingOption,
     });
     nextStep();
-    console.log(data);
   };
 
   return (
     <form className="checkout__form" onSubmit={handleSubmit}>
       <h2 className="checkout__form-title">Shipping Address</h2>
-      <div className="checkout__form-fields">
-        <FormInput name="firstName" placeholder="First name" />
-        <FormInput name="lastName" placeholder="Last name" />
-        <FormInput name="email" placeholder="Email" />
-        <FormInput name="address" placeholder="Address" />
-        <FormInput name="city" placeholder="City" />
-        <FormInput name="postalCode" placeholder="Postal code" />
-        <select
-          value={shippingCountry}
-          onChange={(e) => setShippingCountry(e.target.value)}
-        >
-          {Object.entries(shippingCountries)
-            .map(([code, name]) => ({
-              id: code,
-              label: name,
-            }))
-            .map((item) => (
-              <option key={item.id} value={item.id}>
-                {item.label.slice(0, 31)}
-              </option>
-            ))}
-        </select>
-        <select
-          value={shippingSubdivision}
-          onChange={(e) => setShippingSubdivision(e.target.value)}
-        >
-          {Object.entries(shippingSubdivisions)
-            .map(([code, name]) => ({
-              id: code,
-              label: name,
-            }))
-            .map((item) => (
-              <option key={item.id} value={item.id}>
-                {item.label}
-              </option>
-            ))}
-        </select>
-        <select
-          value={shippingOption}
-          onChange={(e) => setShippingOption(e.target.value)}
-        >
-          {shippingOptions
-            .map((option) => ({
-              id: option.id,
-              label: `${option.description} - (${option.price.formatted_with_symbol})`,
-            }))
-            .map((item) => (
-              <option key={item.id} value={item.id}>
-                {item.label}
-              </option>
-            ))}
-        </select>
+      <div className="checkout__form-inputs">
+        <FormInput
+          name="firstName"
+          placeholder="First name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.targe.value)}
+        />
+        <FormInput
+          name="lastName"
+          placeholder="Last name"
+          value={lastName}
+          onChange={(e) => setLastName(e.targe.value)}
+        />
+        <FormInput
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.targe.value)}
+        />
+        <FormInput
+          name="address"
+          placeholder="Address"
+          value={address}
+          onChange={(e) => setAddress(e.targe.value)}
+        />
+        <FormInput
+          name="city"
+          placeholder="City"
+          value={city}
+          onChange={(e) => setCity(e.targe.value)}
+        />
+        <FormInput
+          name="postalCode"
+          placeholder="Postal code"
+          value={postalCode}
+          onChange={(e) => setPostalCode(e.targe.value)}
+        />
+      </div>
+      <div className="checkout__form-selects">
+        <div className="checkout__form-select-wrapper">
+          <label htmlFor="shippingCountry">Shipping Country</label>
+          <select
+            value={shippingCountry}
+            onChange={(e) => setShippingCountry(e.target.value)}
+          >
+            {Object.entries(shippingCountries)
+              .map(([code, name]) => ({
+                id: code,
+                label: name,
+              }))
+              .map((item) => (
+                <option key={item.id} value={item.id}>
+                  {item.label.slice(0, 31)}
+                </option>
+              ))}
+          </select>
+        </div>
+        <div className="checkout__form-select-wrapper">
+          <label htmlFor="shippingSubdivision">Shipping Subdivision</label>
+          <select
+            value={shippingSubdivision}
+            onChange={(e) => setShippingSubdivision(e.target.value)}
+          >
+            {Object.entries(shippingSubdivisions)
+              .map(([code, name]) => ({
+                id: code,
+                label: name,
+              }))
+              .map((item) => (
+                <option key={item.id} value={item.id}>
+                  {item.label}
+                </option>
+              ))}
+          </select>
+        </div>
+        <div className="checkout__form-select-wrapper">
+          <label htmlFor="shippingOption">Shipping Option</label>
+          <select
+            value={shippingOption}
+            onChange={(e) => setShippingOption(e.target.value)}
+          >
+            {shippingOptions
+              .map((option) => ({
+                id: option.id,
+                label: `${option.description} - (${option.price.formatted_with_symbol})`,
+              }))
+              .map((item) => (
+                <option key={item.id} value={item.id}>
+                  {item.label}
+                </option>
+              ))}
+          </select>
+        </div>
       </div>
       <div className="checkout__form-actions">
         <Link
