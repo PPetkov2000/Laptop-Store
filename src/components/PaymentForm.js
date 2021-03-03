0104164162a311aaca15c2a44dbc32f59d35d623
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-03-03 23:21:24 +0200

    Update PaymentForm.js

--- src/components/PaymentForm.js
+++ src/components/PaymentForm.js
@@ -1,9 +1,99 @@
 import React from "react";
+import {
+  Elements,
+  CardElement,
+  ElementsConsumer,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import OrderReview from "./OrderReview";
 
-const PaymentForm = () => {
+const stripePromise = loadStripe(
+  "pk_test_51IQyrMCrXr4TyFDuDtgiLhC53AhlJUjxDN8hboY2UN505Egi3F063ij3LUcddSqTQQ2lMokMAmk7IVCwpGzwIcDq00nqBdgEsS"
+);
+
+const PaymentForm = ({
+  checkoutToken,
+  shippingData,
+  backStep,
+  nextStep,
+  onCaptureCheckout,
+}) => {
+  console.log(shippingData);
+  const handleSubmit = async (event, elements, stripe) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) return;
+
+    const cardElement = elements.getElement(CardElement);
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: cardElement,
+    });
+    console.log(paymentMethod);
+
+    if (error) {
+      console.log(error);
+    } else {
+      const orderData = {
+        line_items: checkoutToken.live.line_items,
+        customer: {
+          firstname: shippingData.firstname,
+          lastname: shippingData.lastname,
+          email: shippingData.email,
+        },
+        shipping: {
+          name: "International",
+          street: shippingData.address,
+          town_city: shippingData.city,
+          country: shippingData.shippingCountry,
+          country_state: shippingData.shippingSubdivision,
+          postal_zip_code: shippingData.zip,
+        },
+        fullElement: { shipping_method: shippingData.shippingOption },
+        payment: {
+          gateway: "stripe",
+          stripe: { payment_method_id: paymentMethod.id },
+        },
+      };
+      onCaptureCheckout(checkoutToken.id, orderData);
+      nextStep();
+    }
+  };
+
   return (
-    <div>
-      <h1>Payment Form</h1>
+    <div className="payment-method">
+      <OrderReview checkoutToken={checkoutToken} />
+      <div className="payment-method__wrapper">
+        <h3 className="payment-method__title">Payment Method</h3>
+        <Elements stripe={stripePromise}>
+          <ElementsConsumer>
+            {({ elements, stripe }) => (
+              <form
+                onSubmit={(e) => handleSubmit(e, elements, stripe)}
+                className="payment-method__form"
+              >
+                <CardElement />
+                <div className="payment-method__form-actions">
+                  <button
+                    className="payment-method__form-back-button button-primary-darker"
+                    onClick={backStep}
+                  >
+                    Back
+                  </button>
+                  <button
+                    type="submit"
+                    className="payment-method__form-submit-button button-secondary"
+                    disabled={!stripe}
+                  >
+                    Pay {checkoutToken.live.subtotal.formatted_with_symbol}
+                  </button>
+                </div>
+              </form>
+            )}
+          </ElementsConsumer>
+        </Elements>
+      </div>
     </div>
   );
 };
