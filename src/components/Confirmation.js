41c0e14e85276f1931484862c80f19049b57b30d
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-03-08 19:55:47 +0200

    Update Confirmation.js

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -2,26 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Loader from "./Loader";
 
-const Confirmation = ({ order, error }) => {
-  return !order.customer ? (
+const Confirmation = ({ order, error, isFinished }) => {
+  return isFinished ? (
+    <div className="confirmation">
+      <h4 className="confirmation__title">Thank you for your purchase</h4>
+      <Link to="/" className="confirmation__link button-primary-darker">
+        Back to home
+      </Link>
+    </div>
+  ) : !order.customer ? (
     <Loader />
   ) : error ? (
     <div className="confirmation">
-      <h5 className="confirmation__error">{error}</h5>
-      <Link to="/" className="confirmation__link">
+      <h4 className="confirmation__error">{error}</h4>
+      <Link to="/" className="confirmation__link button-primary-darker">
         Back to home
       </Link>
     </div>
   ) : (
     <div className="confirmation">
-      <h5 className="confirmation__title">
+      <h4 className="confirmation__title">
         Thank you for your purchase, {order.customer.firstname}{" "}
         {order.customer.lastname}
-      </h5>
+      </h4>
       <p className="confirmation__text">
         Order ref: {order.customer_reference}
       </p>
-      <Link to="/" className="confirmation__link">
+      <Link to="/" className="confirmation__link button-primary-darker">
         Back to home
       </Link>
     </div>
