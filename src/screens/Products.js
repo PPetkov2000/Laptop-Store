ab9dd3d8edad825fd47aedadf804c7698ff4fc86
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-03-08 19:53:09 +0200

    Update Products.js

--- src/screens/Products.js
+++ src/screens/Products.js
@@ -1,64 +1,12 @@
-import React, { useState, useEffect } from "react";
-import Loader from "../components/Loader";
+import React from "react";
 import Product from "../components/Product";
-import { commerce } from "../lib/commerce";
 
-const Products = () => {
-  const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  async function fetchProducts() {
-    try {
-      const { data } = await commerce.products.list();
-      setProducts(data);
-      setLoading(false);
-    } catch (error) {
-      setError(error.message);
-      setLoading(false);
-      console.log(error);
-    }
-  }
-
-  async function fetchCart() {
-    try {
-      const data = await commerce.cart.retrieve();
-      setCart(data);
-      setLoading(false);
-    } catch (error) {
-      setError(error.message);
-      setLoading(false);
-      console.log(error);
-    }
-  }
-
-  async function handleAddToCart(productId, quantity) {
-    try {
-      const data = await commerce.cart.add(productId, quantity);
-      console.log(data);
-      setCart(data.cart);
-      setLoading(false);
-    } catch (error) {
-      setError(error.message);
-      setLoading(false);
-      console.log(error);
-    }
-  }
-
-  useEffect(() => {
-    fetchProducts();
-    fetchCart();
-  }, []);
-
+const Products = ({ products, handleAddToCart }) => {
   return (
     <section className="products">
-      <h1 className="products__title">Products</h1>
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        { error }
-      ) : (
+      <div className="content-wrapper">
+        <h1 className="products__title">Products</h1>
+
         <article className="products__wrapper">
           {products.map((product) => (
             <Product
@@ -68,7 +16,7 @@ const Products = () => {
             />
           ))}
         </article>
-      )}
+      </div>
     </section>
   );
 };
