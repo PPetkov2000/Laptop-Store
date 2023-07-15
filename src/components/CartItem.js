52788128d7af540d3f0f493ffb57025493189fdd
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2023-07-16 00:08:24 +0300

    Update CartItem.js

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,21 +1,14 @@
-import React from "react";
+import React from 'react'
 
 const CartItem = ({ item, removeItem }) => {
   return (
     <div className="item__card">
-      <img
-        src={item.media.source}
-        alt={item.name}
-        className="item__card-image"
-      />
+      <img src={item.media.source} alt={item.name} className="item__card-image" loading="lazy" />
       <p className="item__card-text">{item.product_name}</p>
       <p className="item__card-text">{item.price.formatted_with_symbol}</p>
-      <i
-        className="fa fa-trash icon-red item__card-delete-icon"
-        onClick={removeItem}
-      ></i>
+      <i className="fa fa-trash icon-red item__card-delete-icon" onClick={removeItem}></i>
     </div>
-  );
-};
+  )
+}
 
-export default CartItem;
+export default CartItem
