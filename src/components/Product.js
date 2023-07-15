e75f4d88287c84f007dcf5529459d3b30a0e31d2
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2023-07-16 00:08:51 +0300

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,30 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Product = ({ product, addToCart }) => {
   return (
     <div className="product__card">
-      <Link to={`/products/${product.id}`}>
-        <img
-          src={product.media.source}
-          alt={product.name}
-          className="product__card-image"
-        />
+      <Link to={`/products/${product.id}`} aria-label="See product details">
+        <img src={product.media.source} alt={product.name} className="product__card-image" loading="lazy" />
       </Link>
       <div className="product__card-content">
-        <h3 className="product__card-title">
-          {product.name.replaceAll("-", " ")}
-        </h3>
-        <h3 className="product__card-price">
-          {product.price.formatted_with_symbol}
-        </h3>
-        <i
-          className="fa fa-cart-plus product__card-icon"
-          onClick={addToCart}
-        ></i>
+        <h3 className="product__card-title">{product.name.replaceAll('-', ' ')}</h3>
+        <h3 className="product__card-price">{product.price.formatted_with_symbol}</h3>
+        <i className="fa fa-cart-plus product__card-icon icon-secondary" onClick={addToCart}></i>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Product;
+export default Product
